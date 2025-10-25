56c62062227f608dc76604a1d67535f4ffd8acd0
Katydodera <katydodera@gmail.com>
2025-10-25 14:42:30 -0300

    Cambio a Express 4 para deploy en Render

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware para parsear JSON y formularios
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Ruta principal
 app.get('/', (req, res) => {
-  res.send('Hola, SIF-STOCK está corriendo!');
+  res.send('¡SIF-STOCK funcionando!');
 });
 
-// Iniciar servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
